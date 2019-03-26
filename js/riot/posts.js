29ce3cc86acfaa60c95c070e37280684b550014c
AXeL-dev <contact.axel.dev@gmail.com>
2019-03-26 16:21:55 +0100

    fill blog posts

--- js/riot/posts.js
+++ js/riot/posts.js
@@ -6,7 +6,42 @@ var blog_posts = [
         date: 'Mars 26, 2019',
         author: 'AXeL',
         image: 'img/posts/riot-js.png',
-        text: 'Lorem ipsum dolor sit amet, ne usu eius animal, nam accusata conceptam ea.Qui ut prima aliquip reprehendunt, eos ex nibh libris delectus, an has liber melius constituto. Epicuri mediocrem vis ex.'
+        text: `<p>
+            <a href="https://riot.js.org/" target="_blank">Riot</a> is a frontend javascript library that lets you build user interfaces with custom tags using simple and enjoyable syntax. It uses a virtual DOM similar to React but faster. <span class="bold-text">Riot</span> is very tiny compared to its competitors.
+        </p>
+        <br>
+        <p>
+            What makes Riot so interesting?<br>
+            <ul class="disc-list">
+                <li class="bold-text">Easy to learn/use</li>
+                <li class="bold-text">Simple & minimalistic</li>
+                <li class="bold-text">Small, but complete</li>
+                <li class="bold-text">Stable & ready for production</li>
+            </ul>
+        </p>
+        <br>
+        <p>
+            As a developer one of the main problems i face with new technologies is the learning curve, and after spending time on learning something new there is no guarantee that you will find some projects to work on with the technologies you just learned, you may be just wasting time on learning something you won't use.
+            This is why i'm trying to focus more on my choices & this is why i chose Riot ;)
+        </p>
+        <br>
+        <p>
+            Convinced & want to give it a try? here you go!<br>
+            <iframe src="https://riot.js.org/examples/live-editor/"></iframe>
+        </p>
+        <br>
+        <p>
+            I recently introduced Riot to my portfolio, you will find the full source code <a href="https://github.com/AXeL-dev/portfolio" target="_blank">here</a> (it stills not the best example out there).
+        </p>
+        `,
+        categories: [
+            'Front End',
+            'Library'
+        ],
+        tags: [
+            'Javascript',
+            'Riot.js'
+        ]
     },
     {
         id: 3,
@@ -14,7 +49,46 @@ var blog_posts = [
         date: 'Mars 25, 2019',
         author: 'AXeL',
         image: 'img/posts/dolibarr.png',
-        text: 'Lorem ipsum dolor sit amet, ne usu eius animal, nam accusata conceptam ea.Qui ut prima aliquip reprehendunt, eos ex nibh libris delectus, an has liber melius constituto. Epicuri mediocrem vis ex.'
+        text: `<p>
+            <a href="https://www.dolibarr.org/" target="_blank">Dolibarr</a> is an opensource and free software package to manage companies, freelancers or foundations. It's basically a web application coded in javascript & PHP that you can use to handle your business on a local or remote server.
+        </p>
+        <br>
+        <p>
+            I've been working on Dolibarr for almost 3 years now and here is what i think about it.
+        </p>
+        <br>
+        <p>
+            The good part:<br>
+            <ul class="disc-list">
+                <li><span class="bold-text">Free & opensource</span> (even if the most of external modules are paid)</li>
+                <li class="bold-text">Simple installation</li>
+                <li class="bold-text">Ergonomic & user friendly interface</li>
+                <li class="bold-text">Extensible & customisable</li>
+                <li><span class="bold-text">Growing audience</span> (dolibarr is widely used in some countries like France & Greece)</li>
+            </ul>
+        </p>
+        <br>
+        <p>
+            The really bad part:<br>
+            <ul class="disc-list">
+                <li><span class="bold-text">Bugs</span> (you can easily discover a bug just by playing with your browser address bar)</li>
+                <li><span class="bold-text">Spaghetti code</span> (no wonder why there is so much bugs..)</li>
+                <li><span class="bold-text">Poor documentation</span> (developer documentation is really old & outdated)</li>
+                <li><span class="bold-text">Slow development</span> (even if dolibarr has seen very good changes in the last years)</li>
+            </ul>
+        </p>
+        <br>
+        <p>
+            As a conclusion, i can say as a developer that a full-rewrite using the new web technologies is maybe required to get a clean & easy to understand base code. Also, providing a good documentation is fundamental, especially for newcomers.
+        </p>`,
+        categories: [
+            'ERP',
+            'CRM'
+        ],
+        tags: [
+            'PHP',
+            'Dolibarr'
+        ]
     },
     {
         id: 2,
@@ -22,7 +96,42 @@ var blog_posts = [
         date: 'Mars 25, 2019',
         author: 'AXeL',
         image: 'img/posts/codeigniter.png',
-        text: 'Lorem ipsum dolor sit amet, ne usu eius animal, nam accusata conceptam ea.Qui ut prima aliquip reprehendunt, eos ex nibh libris delectus, an has liber melius constituto. Epicuri mediocrem vis ex.'
+        text: `<p>
+            <a href="https://www.codeigniter.com/" target="_blank">CodeIgniter</a> is one of the most popular PHP frameworks out there. Even though its activity & improvements have been too slow the last years, it stays simple, easy to learn, flexible & the most important it's blazing fast!
+        </p>
+        <br>
+        <p>
+            Version 4 is a complete rewrite of the framework, so let's check what's new:<br><br>
+            <ul class="disc-list">
+                <li>
+                    <p class="bold-text">The welcome page got a pretty new design<p>
+                    <img src="img/posts/codeigniter4-welcome.png" alt="CodeIgniter4" />
+                </li>
+                <li>
+                    <p class="bold-text">A debug bar has been introduced also</p>
+                    <img src="img/posts/codeigniter4-debugbar.png" alt="CodeIgniter4" />
+                </li>
+                <li>
+                    <p class="bold-text">The error parser has a new nice look & got some improvements</p>
+                    <img src="img/posts/codeigniter4-errorparser.png" alt="CodeIgniter4" />
+                </li>
+                <li class="bold-text">Introduction of namespaces</li>
+                <li class="bold-text">Support for newer PHP versions & functionalities</li>
+            </ul>
+        </p>
+        <br>
+        <p>
+            <span class="bold-text">Note</span>: version 4 is currently in the beta stage, wich means that it's not yet ready for production, but it won't last for long.
+        </p>
+        `,
+        categories: [
+            'Back End',
+            'Framework'
+        ],
+        tags: [
+            'PHP',
+            'CodeIgniter'
+        ]
     },
     {
         id: 1,
@@ -31,31 +140,26 @@ var blog_posts = [
         author: 'AXeL',
         image: 'img/posts/lynda.com.jpg',
         text: `<p>
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-            Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-            Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
-            Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
-            imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.
-            Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula,
-            porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis,
-            feugiat a, tellus.
+            <a href="https://www.lynda.com/" target="_blank">Lynda.com</a> is an online learning platform offering educational training courses in the fields of business, software, technology, and creative skills. It is a subsidiary of Linkedin.
         </p>
-        <img src="img/posts/post.jpg" alt="post" />
+        <br>
         <p>
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-            Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-            Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+            You can get a free account by following those steps:
+            <ul>
+                <li><span class="bold-text">1</span>. Request a library card from <a href="https://know.freelibrary.org/MyResearch/register/policy" target="_blank">freelibrary.org</a> (or <a href="https://www.dclibrary.org/catalog/account/application" target="_blank">dclibrary.org</a>)</li>
+                <li><span class="bold-text">2</span>. Accept the policy</li>
+                <li><span class="bold-text">3</span>. Fill the registration form (for the Home Address I recommend you to look at <a href="https://libwww.freelibrary.org/locations/" target="_blank">locations</a>)</li>
+                <li><span class="bold-text">4</span>. Check your mail after validation, and copy the login informations</li>
+                <li><span class="bold-text">5</span>. Login to lynda.com with Organization Portal (<a href="https://www.lynda.com/signin/organization" target="_blank">Sign in with your organization portal</a>), type "freelibrary.org"</li>
+                <li><span class="bold-text">6</span>. Fill your login informations from your mail</li>
+            </ul>
         </p>
-        <blockquote>
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-            Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-        </blockquote>
+        <br>
         <p>
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-            Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-            Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
-            Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
-            imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.
-        </p>`
+            If you don't have an email you can simply use <a href="https://temp-mail.org/" target="_blank">temp-mail.org</a>. You can also use <a href="https://www.fakenamegenerator.com/" target="_blank">fakenamegenerator.com</a> for personal informations. Happy Learning.
+        </p>`,
+        tags: [
+            'e-learning'
+        ]
     }
 ];
