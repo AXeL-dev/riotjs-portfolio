a1992b901a67afce93159ca7892585fabc31fb7d
AXeL-dev <contact.axel.dev@gmail.com>
2019-03-27 13:25:27 +0100

    add php vs node post

--- js/riot/posts.js
+++ js/riot/posts.js
@@ -1,6 +1,58 @@
 
 var blog_posts = [
     {
+        id: 7,
+        title: 'PHP vs node.js',
+        date: 'March 27, 2019',
+        author: 'AXeL',
+        image: 'img/posts/php-vs-node.jpg',
+        text: `
+        <p>
+            Web development is a diverse and ever-changing landscape. New tools and technologies are emerging regularly, making developers face a dilemma: whether to choose long-standing and mature solutions or to give newcomers a try.
+        </p>
+        <p class="mgt-10">
+            Back-end developers often deal with a problem of choosing between Node.js and PHP. This has become a common question lately, though earlier JavaScript, which underlies Node.js, didn’t overlap with PHP. JavaScript built front-end applications, while PHP was used for server-side development. Working together, these two languages created dynamic websites.
+        </p>
+        <p class="mgt-10">
+            Why did the situation change? This happened because JS represented by a new Node.js started entering a server-side sphere and distract back-end developers from old-fashioned PHP.
+        </p>
+        <p class="mgt-10">
+            <p class="bold-text">Comparison of Node.js and PHP</p>
+            <div class="center-align">
+                <img src="img/posts/php-vs-node-cmp.png" alt="PHP vs Node.js" />
+            </div>
+            <p>It is quite difficult to choose a winner in PHP or Node.js on the basis of their features and advantages. It would be better to build different test cases for them and carry them out against a benchmark on the same machine. This way you’ll see how they perform in equal conditions, and will be able to choose the best option for your project.</p>
+        </p>
+        <p class="mgt-10">
+            <p class="bold-text">When should you use PHP or Node.js?</p>
+            <p class="mgt-10">In spite of all the advantages and disadvantages, there’s still no ubiquitous answer as to the best solution. The technology for each backend project should be chosen individually on the basis of its specific features.</p>
+            <p class="mgt-10">Choose PHP if your project includes:</p>
+            <ul class="disc-list mgt-10">
+                <li>CMS such as WordPress, Drupal, Joomla, etc</li>
+                <li>Servers such as MySQL, SQL, MariaDB, Oracle, Sybase, Postgresql, etc</li>
+                <li>Solution stacks such as LAMP (Linux, Apache, MySQL, PHP)</li>
+            </ul>
+            <p class="mgt-10">Choose Node.js if your project includes:</p>
+            <ul class="disc-list mgt-10">
+                <li>Streaming data transfer</li>
+                <li>Real-time web development</li>
+                <li>Software stacks like MEAN (mongoDB, Express.js, Angular.js, Node.js)</li>
+                <li>Built-in expansion capabilities (cluster package)</li>
+            </ul>
+        </p>
+        <p class="mgt-10">
+            <a href="https://www.sam-solutions.com/blog/node-js-vs-php-which-is-the-best-for-server-side-development/" target="_blank">Source</a>
+        </p>
+        `,
+        categories: [
+            'Comparison'
+        ],
+        tags: [
+            'PHP',
+            'node.js'
+        ]
+    },
+    {
         id: 6,
         title: 'Magento CMS: Presentation',
         date: 'March 27, 2019',
@@ -41,6 +93,9 @@ var blog_posts = [
             <p class="bold-text">Conclusion</p>
             <p class="mgt-10">Regardless of its cons, the pros cancel the cons out. Hence, Magento received the most public demand out of all eCommerce platforms in 2018. Magento CMS is a different take from the rest of the crowd. They offer benefits which other platforms are unable to offer.</p>
             <p class="mgt-10">All the advantages add up to be the best CMS there is on the market. So, you cannot possibly compare with any other CMS. If anyone who has previously used Magento knows how much sales you can make through Magento. And better sales should be the motive of any eCommerce site.</p>
+        </p>
+        <p class="mgt-10">
+            <a href="https://www.aspirationhosting.com/magento-cms-best-cms/" target="_blank">Source</a>
         </p>
         `,
         categories: [
