1b4b882cf7352f096fbfd66b28f50a6984799992
AXeL-dev <contact.axel.dev@gmail.com>
2019-05-18 16:52:42 +0000

    update posts

--- js/riot/posts.js
+++ js/riot/posts.js
@@ -14,7 +14,7 @@ var blog_posts = [
             So.. it have been 2 years now or maybe a little more since i'm doing some freelancing tasks & compared to my previous jobs career "especially in my country" i could say that i was <b>a loser</b>.. okay, so why am i saying this? what's the point?
         </p>
         <p class="mgt-10">
-            The point is if you don't work hard to achieve your dreams, someone else will hire you to achieve theirs. And when it comes to dreams they could ask you to build a plane while you don't even know how to build a car.. You get me now?
+            The point is if you don't work hard to achieve your dreams, someone else will hire you to achieve theirs. And when it comes to dreams they could ask you to build a plane while you don't even know how to build a car.. You get me, right?
         </p>
         <p class="mgt-10">
             Okay..! so what's the benefits of freelance:
@@ -29,12 +29,12 @@ var blog_posts = [
             Cool! what about the cons? yup, there is always some:
         </p>
         <ul class="disc-list mgt-10">
-            <li><b>No guarantee</b> (it's not easy to find some tasks or projects to work on)</li>
-            <li><b>Laziness</b>, wasting time sleeping or doing nothing.. (just do something ! doing some sports, yoga or any kind of activity could help)</li>
+            <li><b>No guarantee</b> (it's not easy to find some tasks or projects to work on, unless you have some experience)</li>
+            <li><b>Laziness</b>, wasting time sleeping or doing nothing.. (yeah i know.. so just do something ! doing some sports, yoga or any kind of activity could help)</li>
             <li><b>Depression</b>: if you feel depressed while working/being alone, it's totally normal, we "humans" are social by nature --', so just talk to someone (even if you keep talking to yourself ^^')</li>
         </ul>
         <p class="mgt-10">
-            All this said, only you could define if you'll be a successful or an unsuccessful freelancer, the same as, only you can decide to be a good or bad person.. so even if you have no significant experience(s), just carry on, find some projects to work on, there is a plenty of opensource projects & opportunities waiting for you out there, and show your know-how & passion to the world !
+            All this said, only you could define if you'll be a successful or an unsuccessful freelancer, the same as, only you can decide to be a good or bad person.. so even if you have no significant experience(s), just carry on, find some projects to work on, there is a plenty of open source projects & opportunities waiting for you out there, and show your know-how & passion to the world !
         </p>
         `,
         categories: [
