c3b414bc65f2ec43069df076719c9ca9292e4f7d
AXeL-dev <contact.axel.dev@gmail.com>
2019-05-25 00:46:49 +0000

    add gitflow blog post

--- js/riot/posts.js
+++ js/riot/posts.js
@@ -1,6 +1,38 @@
 
 var blog_posts = [
     {
+        id: 10,
+        title: 'Gitflow: A successful branching model',
+        date: 'May 25, 2019',
+        author: 'AXeL',
+        image: 'img/posts/gitflow-mini.png',
+        text: `
+        <p>
+            <a href="https://github.com/petervanderdoes/gitflow-avh" target="_blank">Gitflow</a> is a git branching and release management workflow that helps developers keep track of features, hotfixes and releases in bigger software projects. This workflow has lot of commands to type and remember, so there’s a <a href="https://github.com/petervanderdoes/gitflow-avh#git-flow-usage">library of git subcommands</a> to help automate some parts of the flow to make working with it easier.
+        </p>
+        <p class="mgt-10">
+            <img src="img/posts/gitflow.png" alt="Gitflow" />
+        </p>
+        <p class="mgt-10">
+            Next is to install the git-flow package & start using it in your projects. You'll find more details in the links below:
+        </p>
+        <ul class="disc-list mgt-10">
+            <li><a href="https://github.com/petervanderdoes/gitflow-avh/wiki/Installation">Installation instructions</a></li>
+            <li><a href="https://github.com/petervanderdoes/gitflow-avh#git-flow-usage">Usage</a></li>
+            <li><a href="https://nvie.com/posts/a-successful-git-branching-model/">Learn more</a></li>
+        </ul>
+        <p class="mgt-10">
+            <a href="https://jeffkreeftmeijer.com/git-flow/">Source</a>
+        </p>
+        `,
+        categories: [
+            'Workflow'
+        ],
+        tags: [
+            'Git'
+        ]
+    },
+    {
         id: 9,
         title: 'Why being a freelancer is better !',
         date: 'May 18, 2019',
