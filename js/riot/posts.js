484dde414fc35c4c183fb136365ee9fe0de3c8ee
AXeL-dev <contact.axel.dev@gmail.com>
2019-06-29 12:49:41 +0100

    Added osTicket blog post

--- js/riot/posts.js
+++ js/riot/posts.js
@@ -1,6 +1,33 @@
 
 var blog_posts = [
     {
+        id: 13,
+        title: 'osTicket: An open source ticket manager',
+        date: 'June 23, 2019',
+        author: 'AXeL',
+        image: 'img/posts/osticket.png',
+        text: `
+        <p>
+            <a href="https://github.com/osTicket" target="_blank">osTicket</a> is a widely-used open source support ticket system brought to you by <a href="https://osticket.com/" target="_blank">osticket.com</a>. It seamlessly integrates inquiries created via email and web-based forms into a simple easy to use multi-user web interface. Easily manage, organize and archive all your support requests and responses in one place while providing your clients with accountability and responsiveness they deserve.
+        </p>
+        <p class="mgt-10">
+            <img src="img/posts/osticket-theme.png" alt="osTicket" />
+        </p>
+        <p class="mgt-10">
+            <span class="bold-text">(*) Note:</span> the above image shows a paid theme for osTicket client side. Check this <a href="https://osticketthemes.com/demo/">demo</a> for more details.
+        </p>
+        <p class="mgt-10">
+            You can download osTicket for free <a href="https://osticket.com/download/">here</a>. You can also enhance it with your own set of plugins & themes.
+        </p>
+        <p class="mgt-10">
+            <a href="http://www.ostickethacks.com/demo/demo_info.php">Source</a>
+        </p>
+        `,
+        tags: [
+            'osTicket'
+        ]
+    },
+    {
         id: 12,
         title: 'Discover LDAP',
         date: 'June 22, 2019',
