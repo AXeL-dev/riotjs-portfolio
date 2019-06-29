f22206a8140e6bca65b777e0db9bac6553964ab3
AXeL-dev <contact.axel.dev@gmail.com>
2019-06-29 12:13:08 +0100

    Added LDAP blog post

--- js/riot/posts.js
+++ js/riot/posts.js
@@ -1,6 +1,32 @@
 
 var blog_posts = [
     {
+        id: 12,
+        title: 'Discover LDAP',
+        date: 'June 22, 2019',
+        author: 'AXeL',
+        image: 'img/posts/ldap.png',
+        text: `
+        <p>
+            <a href="https://en.wikipedia.org/wiki/Lightweight_Directory_Access_Protocol" target="_blank">LDAP</a> is an internet protocol mainly used to centralise data. A good use case example of LDAP is centralised authentication, which simply consist on using the same username & password to access any sort of application inside the LDAP circle. Surely, this is not the only purpose of LDAP, it could even be used to centralise any kind of data so you can access them anytime & anywhere right in the same place.
+        </p>
+        <p class="mgt-10">
+            <img src="img/posts/ldap-auth.jpg" alt="LDAP" />
+        </p>
+        <p class="mgt-10">
+            If you are interested in LDAP & you want to give it a try, the links below could help you:
+        </p>
+        <ul class="disc-list mgt-10">
+            <li><a href="https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-openldap-and-phpldapadmin-on-ubuntu-16-04">Install and Configure OpenLDAP on Ubuntu</a></li>
+            <li><a href="https://github.com/leenooks/phpLDAPadmin">phpLDAPadmin</a></li>
+            <li><a href="https://en.wikipedia.org/wiki/Active_Directory">Active Directory</a></li>
+        </ul>
+        `,
+        tags: [
+            'LDAP'
+        ]
+    },
+    {
         id: 11,
         title: 'Run Photoshop CS6 on Linux',
         date: 'June 15, 2019',
